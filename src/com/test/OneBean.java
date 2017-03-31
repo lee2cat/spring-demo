69f8e71bd89acf16686a37acce2cc3c4d6d2b54a
test <test@qq.com>
2017-03-31 15:50:14 +0800

    first commit

---
+++ src/com/test/OneBean.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by cjp on 2017/3/31.
+ */
+public class OneBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+        System.out.println(applicationContext.getBean("oneBean"));
+    }
+}
